b574c272d8f16c6e18aef1aecab5fbdd3e5cb955
Paulo Mouat <paulo.mouat@gmail.com>
2012-09-14 18:30:00 -0400

    Refactored Identity to IdentityMonad and added relevant helper methods.

--- dotnet/MonadExperiments/Identity.cs
+++ dotnet/MonadExperiments/Identity.cs
@@ -13,16 +13,32 @@ namespace MonadExperiments
 		}
 	}
 
+	public static class IdentityMonad
+	{
+		public static Identity<A> Unit<A>(A a)
+		{
+			Console.WriteLine("Constructing a new Identity<{0}> with value '{1}'...", typeof(A), a);
+			return new Identity<A>(a);
+		}
+
+		public static Identity<B> Bind<A, B>(Identity<A> ma, Func<A, Identity<B>> f)
+		{
+			Console.WriteLine("Binding Identity<{0}> with value '{1}' to function from '{2}' to Identity<{3}>...",
+				typeof(A), ma.Value, typeof(A), typeof(B));
+			return f(ma.Value);
+		}
+	}
+
 	public static class IdentityMonadExtensions
 	{
 		public static Identity<A> IdentityUnit<A>(this A a)
 		{
-			return new Identity<A>(a);
+			return IdentityMonad.Unit(a);
 		}
 
 		public static Identity<B> Bind<A, B>(this Identity<A> ma, Func<A, Identity<B>> f)
 		{
-			return f(ma.Value);
+			return IdentityMonad.Bind(ma, f);
 		}
 	}
 }
