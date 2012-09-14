e36d05011dc40b12ad0a3ce0db420224044ac1a4
Paulo Mouat <paulo.mouat@gmail.com>
2012-09-14 18:28:04 -0400

    Refactored Identity to IdentityMonad and added relevant helper methods.

--- dotnet/MonadExperiments/Program.cs
+++ dotnet/MonadExperiments/Program.cs
@@ -16,47 +16,56 @@ namespace MonadExperiments
 		private void UseIdentityMonad()
 		{
 			Console.WriteLine("Using IdentityMonad:");
-			var x = 10;
-			var y = 20;
-			var r1 = x.IdentityUnit().Bind(
-				s1 => y.IdentityUnit().Bind(
+			var mx1 = 10.IdentityUnit();
+			var my1 = 20.IdentityUnit();
+			var r1 = mx1.Bind(
+				s1 => my1.Bind(
 					t1 => (s1 + t1).IdentityUnit()));
-			Console.WriteLine("{0} + {1} = {2}", x, y, r1.Value);
-			x = 5;
-			y = 7;
-			var r2 = x.IdentityUnit().Bind(
-				s2 => y.IdentityUnit().Bind(
+			Console.WriteLine("{0} + {1} = {2}", mx1.Value, my1.Value, r1.Value);
+	
+			var mx2 = 5.IdentityUnit();
+			var my2 = 7.IdentityUnit();
+			var r2 = mx2.Bind(
+				s2 => my2.Bind(
 					t2 => (s2 + t2).IdentityUnit()));
-			Console.WriteLine("{0} + {1} = {2}", x, y, r2.Value);
+			Console.WriteLine("{0} + {1} = {2}", mx2.Value, my2.Value, r2.Value);
+
+			var mx3 = IdentityMonad.Unit(11);
+			var my3 = IdentityMonad.Unit(13);
+			var r3 = mx3.Bind(
+				s3 => my3.Bind(
+					t3 => IdentityMonad.Unit(s3 + t3)));
+			Console.WriteLine("{0} + {1} = {2}", mx3.Value, my3.Value, r3.Value);
+	
 		}
 
 		private void UseMaybeMonad()
 		{
 			Console.WriteLine("Using MaybeMonad:");
 
-			var x = "x";
-			var y = 17;
-			var z = new DateTime(2000, 1, 2, 3, 5, 6);
-			var w = Maybe<object>.Nothing;
+			var mx = "x".MaybeUnit();
+			var my = 17.MaybeUnit();
+			var mz = (new DateTime(2000, 1, 2, 3, 5, 6)).MaybeUnit();
+			var mw = Maybe<object>.Nothing;
 			
-			var r1 = x.MaybeUnit().Bind(
-				s1 => y.MaybeUnit().Bind(
-					t1 => (string.Format("x = '{0}', y = '{1}'", x, y)).MaybeUnit())
+			var r1 = mx.Bind(
+				s1 => my.Bind(
+					t1 => (string.Format("x = '{0}', y = '{1}'", mx.Value, my.Value)).MaybeUnit())
 				);
 			Console.WriteLine("r1 = '{0}'", r1.Value);
 
-			var r2 = x.MaybeUnit().Bind(
-				s2 => y.MaybeUnit().Bind(
-					t2 => z.MaybeUnit().Bind(
-						u2 => (string.Format("x = '{0}', y = '{1}', z = '{2}'", x, y, z)).MaybeUnit())
+			var r2 = mx.Bind(
+				s2 => my.Bind(
+					t2 => mz.Bind(
+						u2 => (string.Format("x = '{0}', y = '{1}', z = '{2}'", mx.Value, my.Value, mz.Value)).MaybeUnit())
 					)
 				);
 			Console.WriteLine("r2 = '{0}'", r2.Value);
 
-			var r3 = x.MaybeUnit().Bind(
-				s3 => w.Bind(
-					t3 => z.MaybeUnit().Bind(
-						u3 => (string.Format("x = '{0}', w = '{1}', z = '{2}'", x, w, z)).MaybeUnit())
+			var r3 = mx.Bind(
+				s3 => mw.Bind(
+					t3 => mz.Bind(
+						u3 => (string.Format("x = '{0}', w = '{1}', z = '{2}'", mx.Value, mw.Value, mz.Value)).MaybeUnit())
 					)
 				);
 			Console.WriteLine("r3.HasValue = '{0}', r3 = '{1}'", r3.HasValue, r3.Value);
