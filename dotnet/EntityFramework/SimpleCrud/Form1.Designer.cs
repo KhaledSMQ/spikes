d779eed88f0dfb520162f6706572732ea78b8290
Paulo Mouat <paulo.mouat@gmail.com>
2012-11-14 10:29:25 -0500

    Moved ComingledLists under Wpf. Added EF projects.

---
+++ dotnet/EntityFramework/SimpleCrud/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SimpleCrud
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.ListPeople = new System.Windows.Forms.Button();
+			this.AddProductModel = new System.Windows.Forms.Button();
+			this.AddChildProduct = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.DeleteChildProduct = new System.Windows.Forms.Button();
+			this.InsertButton = new System.Windows.Forms.Button();
+			this.UpdateButton = new System.Windows.Forms.Button();
+			this.DeleteButton = new System.Windows.Forms.Button();
+			this.SelectButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(12, 12);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(635, 225);
+			this.listBox1.TabIndex = 0;
+			// 
+			// ListPeople
+			// 
+			this.ListPeople.Location = new System.Drawing.Point(12, 243);
+			this.ListPeople.Name = "ListPeople";
+			this.ListPeople.Size = new System.Drawing.Size(75, 23);
+			this.ListPeople.TabIndex = 1;
+			this.ListPeople.Text = "List people";
+			this.ListPeople.UseVisualStyleBackColor = true;
+			this.ListPeople.Click += new System.EventHandler(this.ListPeople_Click);
+			// 
+			// AddProductModel
+			// 
+			this.AddProductModel.Location = new System.Drawing.Point(94, 244);
+			this.AddProductModel.Name = "AddProductModel";
+			this.AddProductModel.Size = new System.Drawing.Size(112, 23);
+			this.AddProductModel.TabIndex = 2;
+			this.AddProductModel.Text = "Add product model";
+			this.AddProductModel.UseVisualStyleBackColor = true;
+			this.AddProductModel.Click += new System.EventHandler(this.AddProductModel_Click);
+			// 
+			// AddChildProduct
+			// 
+			this.AddChildProduct.Location = new System.Drawing.Point(213, 244);
+			this.AddChildProduct.Name = "AddChildProduct";
+			this.AddChildProduct.Size = new System.Drawing.Size(112, 23);
+			this.AddChildProduct.TabIndex = 3;
+			this.AddChildProduct.Text = "Add child product";
+			this.AddChildProduct.UseVisualStyleBackColor = true;
+			this.AddChildProduct.Click += new System.EventHandler(this.AddChildProduct_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(331, 249);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "label1";
+			// 
+			// DeleteChildProduct
+			// 
+			this.DeleteChildProduct.Location = new System.Drawing.Point(13, 273);
+			this.DeleteChildProduct.Name = "DeleteChildProduct";
+			this.DeleteChildProduct.Size = new System.Drawing.Size(111, 23);
+			this.DeleteChildProduct.TabIndex = 5;
+			this.DeleteChildProduct.Text = "Delete child product";
+			this.DeleteChildProduct.UseVisualStyleBackColor = true;
+			this.DeleteChildProduct.Click += new System.EventHandler(this.DeleteChildProduct_Click);
+			// 
+			// InsertButton
+			// 
+			this.InsertButton.Location = new System.Drawing.Point(13, 303);
+			this.InsertButton.Name = "InsertButton";
+			this.InsertButton.Size = new System.Drawing.Size(75, 23);
+			this.InsertButton.TabIndex = 6;
+			this.InsertButton.Text = "Insert";
+			this.InsertButton.UseVisualStyleBackColor = true;
+			this.InsertButton.Click += new System.EventHandler(this.Insert_Click);
+			// 
+			// UpdateButton
+			// 
+			this.UpdateButton.Location = new System.Drawing.Point(94, 303);
+			this.UpdateButton.Name = "UpdateButton";
+			this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+			this.UpdateButton.TabIndex = 7;
+			this.UpdateButton.Text = "Update";
+			this.UpdateButton.UseVisualStyleBackColor = true;
+			this.UpdateButton.Click += new System.EventHandler(this.Update_Click);
+			// 
+			// DeleteButton
+			// 
+			this.DeleteButton.Location = new System.Drawing.Point(175, 303);
+			this.DeleteButton.Name = "DeleteButton";
+			this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+			this.DeleteButton.TabIndex = 8;
+			this.DeleteButton.Text = "Delete";
+			this.DeleteButton.UseVisualStyleBackColor = true;
+			this.DeleteButton.Click += new System.EventHandler(this.Delete_Click);
+			// 
+			// SelectButton
+			// 
+			this.SelectButton.Location = new System.Drawing.Point(256, 303);
+			this.SelectButton.Name = "SelectButton";
+			this.SelectButton.Size = new System.Drawing.Size(75, 23);
+			this.SelectButton.TabIndex = 9;
+			this.SelectButton.Text = "Select";
+			this.SelectButton.UseVisualStyleBackColor = true;
+			this.SelectButton.Click += new System.EventHandler(this.Select_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(660, 332);
+			this.Controls.Add(this.SelectButton);
+			this.Controls.Add(this.DeleteButton);
+			this.Controls.Add(this.UpdateButton);
+			this.Controls.Add(this.InsertButton);
+			this.Controls.Add(this.DeleteChildProduct);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.AddChildProduct);
+			this.Controls.Add(this.AddProductModel);
+			this.Controls.Add(this.ListPeople);
+			this.Controls.Add(this.listBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button ListPeople;
+		private System.Windows.Forms.Button AddProductModel;
+		private System.Windows.Forms.Button AddChildProduct;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button DeleteChildProduct;
+		private System.Windows.Forms.Button InsertButton;
+		private System.Windows.Forms.Button UpdateButton;
+		private System.Windows.Forms.Button DeleteButton;
+		private System.Windows.Forms.Button SelectButton;
+	}
+}
+
---
+++ dotnet/EntityFramework/SimpleCrud/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCrud
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void ListPeople_Click(object sender, EventArgs e)
+		{
+			//ApplyChangesToMultiple();
+			RunNormalQuery();
+			//RevertChangesFromMultiple();
+		}
+
+		private void ApplyChangesToMultiple()
+		{
+			using (var context = new AdventureWorks2012Entities())
+			{
+				var people = from p in context.People
+							 where p.LastName == "King"
+							 orderby p.LastName, p.FirstName descending
+							 select p;
+				foreach (var person in people)
+				{
+					person.FirstName = person.FirstName + " - 2";
+				}
+
+				context.SaveChanges();
+			}
+		}
+
+		private void RevertChangesFromMultiple()
+		{
+			using (var context = new AdventureWorks2012Entities())
+			{
+				var people = from p in context.People
+							 where p.LastName == "King"
+							 orderby p.LastName, p.FirstName descending
+							 select p;
+				foreach (var person in people)
+				{
+					var firstName = person.FirstName;
+					person.FirstName = firstName.Substring(0, firstName.Length - 4);
+				}
+
+				context.SaveChanges();
+			}
+		}
+
+		private void RunNormalQuery()
+		{
+			using (var context = new AdventureWorks2012Entities())
+			{
+				var people = from p in context.People
+							 where p.LastName == "King" || p.LastName == "Jones"
+							 orderby p.LastName, p.FirstName descending
+							 select new { p.FirstName, p.LastName };
+				/*var people =
+					context.People.
+						Where(p => p.LastName == "King").
+						OrderByDescending(p => p.FirstName).
+						Select(p => new {p.FirstName, p.LastName});*/
+				foreach (var person in people)
+				{
+					listBox1.Items.Add(string.Format("{0} {1}", person.FirstName, person.LastName));
+				}
+			}
+		}
+
+		private void RunGroupQuery()
+		{
+			using (var context = new AdventureWorks2012Entities())
+			{
+				var people = from p in context.People
+							 orderby p.FirstName
+							 where p.LastName == "King" || p.LastName == "Jones"
+							 group new { p.FirstName, p.LastName } by p.LastName;
+				foreach (var group in people)
+				{
+					foreach (var person in group)
+					{
+						listBox1.Items.Add(string.Format("{0} {1}", person.FirstName, person.LastName));
+					}
+				}
+			}
+		}
+
+		private void AddProductModel_Click(object sender, EventArgs e)
+		{
+			//AddProductModelMethodTwo();
+		}
+
+		private void AddProductModelMethodOne()
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities1())
+				{
+					var productModel = new ProductModel();
+					productModel.Name = "Front Forks";
+					productModel.rowguid = Guid.NewGuid();
+					productModel.ModifiedDate = DateTime.Now;
+					context.ProductModels.AddObject(productModel);
+					context.SaveChanges();
+					label1.Text = "Save successful";
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				throw;
+			}
+		}
+
+		private void AddProductModelMethodTwo()
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities1())
+				{
+					var productModel = ProductModel.CreateProductModel(0, "Rear Shock", Guid.NewGuid(), DateTime.Now);
+					context.AddToProductModels(productModel);
+					context.SaveChanges();
+					label1.Text = "Save successful";
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				throw;
+			}
+		}
+
+		private void AddChildProduct_Click(object sender, EventArgs e)
+		{
+			AddChildProductExample();
+		}
+
+		private void AddChildProductExample()
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities1())
+				{
+					var prodMod = context.ProductModels.Where(pm => pm.ProductModelID == 129).First();
+					var prod = new Product();
+					prod.Name = "Inverted Kayaba";
+					prod.ProductNumber = "IKAYA-R209";
+					prod.MakeFlag = true;
+					prod.FinishedGoodsFlag = true;
+					prod.Color = "Red";
+					prod.SafetyStockLevel = 250;
+					prod.ReorderPoint = 250;
+					prod.StandardCost = 2500;
+					prod.ListPrice = 3900;
+					prod.Size = "40M";
+					prod.SizeUnitMeasureCode = "CM";
+					prod.WeightUnitMeasureCode = "LB";
+					prod.Weight = (decimal)45.2;
+					prod.DaysToManufacture = 5;
+					prod.ProductLine = "S";
+					prod.Class = "M";
+					prod.Style = "M";
+					prod.ProductSubcategoryID = 1;
+					prod.SellStartDate = DateTime.Now;
+					prod.ModifiedDate = DateTime.Now;
+					prod.rowguid = Guid.NewGuid();
+					prod.ProductModel = prodMod;
+					context.SaveChanges();
+					label1.Text = "Save successful";
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.InnerException.Message);
+			}
+		}
+
+		private void DeleteChildProduct_Click(object sender, EventArgs e)
+		{
+			DeleteChildProductExample();
+		}
+
+		private void DeleteChildProductExample()
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities1())
+				{
+					var prod = context.Products.Where(p => p.ProductNumber == "IKAYA-R209").First();
+					context.DeleteObject(prod);
+					context.SaveChanges();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.InnerException.Message);
+			}
+		}
+
+		private void Insert_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities())
+				{
+					var busent = context.BusinessEntities.Where(p => p.BusinessEntityID == 292).First();
+					var per = new Person();
+					per.PersonType = "SC";
+					per.NameStyle = true;
+					per.Title = "Geek";
+					per.FirstName = "Scott";
+					per.MiddleName = "L";
+					per.LastName = "Klein";
+					per.Suffix = "Mr";
+					per.EmailPromotion = 1;
+					per.rowguid = Guid.NewGuid();
+					per.ModifiedDate = DateTime.Now;
+					busent.Person = per;
+					context.SaveChanges();
+					MessageBox.Show("record inserted");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.InnerException.Message);
+			}
+		}
+
+		private void Update_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities())
+				{
+					var per = context.People.Where(p => p.BusinessEntityID == 292).First();
+					per.Title = "Head Geek";
+					per.ModifiedDate = DateTime.Now;
+					per.PersonType = "EM";
+					context.SaveChanges();
+					MessageBox.Show("record updated");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.InnerException.Message);
+			}
+		}
+
+		private void Delete_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (var context = new AdventureWorks2012Entities())
+				{
+					var per = context.People.Where(p => p.BusinessEntityID == 292).First();
+					context.DeleteObject(per);
+					context.SaveChanges();
+					MessageBox.Show("record deleted");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void Select_Click(object sender, EventArgs e)
+		{
+			using (var context = new AdventureWorks2012Entities())
+			{
+				var query = from p in context.SelectPeople()
+								where p.LastName.StartsWith("Kl")
+								select p;
+				foreach (var per in query)
+				{
+					listBox1.Items.Add(string.Format("{0} {1}", per.FirstName, per.LastName));
+				}
+			}
+		}
+	}
+}
